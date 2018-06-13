ef8b316cf4b41aa2805d127dbff9dd2e87d1cf2f
mdontsov <mdontsov@live.com>
2018-06-13 21:42:18 +0300

    ...

--- app/src/main/java/com/example/maxim/myapplication/DisplayMessageActivity.kt
+++ app/src/main/java/com/example/maxim/myapplication/DisplayMessageActivity.kt
@@ -1,8 +1,10 @@
 package com.example.maxim.myapplication
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_display_message.*
 
 class DisplayMessageActivity : AppCompatActivity() {
 
@@ -14,6 +16,24 @@ class DisplayMessageActivity : AppCompatActivity() {
 
         val textView = findViewById<TextView>(R.id.newTextView).apply {
             text = message
+        }
+
+        send_email.setOnClickListener() {
+            val intent = Intent(Intent.ACTION_SEND)
+
+            intent.type = "text/html"
+
+            intent.putExtra(Intent.EXTRA_EMAIL, (arrayOf<String>("")))
+
+            intent.putExtra(Intent.EXTRA_CC, (arrayOf<String>("")))
+
+            intent.putExtra(Intent.EXTRA_BCC, arrayOf<String>(""))
+
+            intent.putExtra(Intent.EXTRA_SUBJECT, "")
+
+            intent.putExtra(Intent.EXTRA_TEXT, "")
+
+            startActivity(intent)
         }
     }
 }

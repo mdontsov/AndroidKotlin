8f1256be129e49f488f7a19d205b4fca67db661a
mdontsov <mdontsov@live.com>
2018-06-14 22:34:14 +0300

    ...

--- app/src/androidTest/java/com/example/maxim/myapplication/SampleTest1.kt
+++ app/src/androidTest/java/com/example/maxim/myapplication/SampleTest1.kt
@@ -2,7 +2,9 @@ package com.example.maxim.myapplication
 
 import android.support.test.espresso.Espresso.onView
 import android.support.test.espresso.action.ViewActions
+import android.support.test.espresso.assertion.ViewAssertions
 import android.support.test.espresso.matcher.ViewMatchers.withId
+import android.support.test.espresso.matcher.ViewMatchers.withText
 import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
 import org.junit.Rule
@@ -13,25 +15,33 @@ import org.junit.runner.RunWith
 class SampleTest1 {
     @Rule
     @JvmField
-    val activity = ActivityTestRule<MainActivity>(MainActivity::class.java)
+    val mainActivity = ActivityTestRule<MainActivity>(MainActivity::class.java)
+
+    val textToInput = "Hello Kotlin!"
 
     @Test
     @Throws(Exception::class)
     fun testInputField() {
         onView(withId(R.id.enterMessage))
                 .perform(ViewActions.clearText())
-                .perform(ViewActions.typeText("Hello Kotlin!"))
+                .perform(ViewActions.typeText(textToInput))
     }
 
     @Test
     @Throws(Exception::class)
     fun testSendMessageButton() {
-
+        testInputField()
+        onView(withId(R.id.sendButton))
+                .perform(ViewActions.click())
+        onView(withId(R.id.newTextView))
+                .check(ViewAssertions.matches(withText(textToInput)))
     }
 
     @Test
     @Throws(Exception::class)
     fun testSendEmail() {
-
+        testSendMessageButton()
+        onView(withId(R.id.send_email))
+                .perform(ViewActions.click())
     }
 }
